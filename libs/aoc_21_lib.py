02fbd8182dd02533aa4d0cf850f58be0f0861d7d
Dominik <Dominik.Schillo@googlemail.com>
2020-09-16 21:36:47 +0200

    aoc21 changes

--- libs/aoc_21_lib.py
+++ libs/aoc_21_lib.py
@@ -4,7 +4,7 @@ import functools
 import itertools
 import re
 import numpy as np
-from typing import List, Match, Pattern
+from typing import List
 from libs.timer import timer
 
 
@@ -19,9 +19,30 @@ def create_matrix(string: str, reg_ex_raw: str):
     return matrix
 
 
-def compose2(f, g):
-    """()"""
-    return lambda x: f(g(x))
+def data_input(filename: str) -> list:
+    """"""
+    with open(filename) as f:
+        file = f.read().replace("#", "1").replace(".", "0").split("\n")
+        input_reg_ex: str = r"(\d+)\/(\d+)\/?(\d*)? "
+        output_reg_ex: str = r" (\d+)\/(\d+)\/(\d+)\/?(\d*)?"
+        return [(create_matrix(dataline, input_reg_ex), create_matrix(dataline, output_reg_ex)) for dataline in file]
+
+
+def complete_instruction_dict(instruction_list: list) -> dict:
+    flips = [identity, flip_horizontal, flip_vertical]
+    rotations = [identity, rotate_90, rotate_180, rotate_270]
+    result = {}
+    for inpt, output in instruction_list:
+        for flip, rotation in itertools.product(flips, rotations):
+            result[tuple(map(tuple, flip(rotation(inpt))))] = output
+    return result
+
+
+def repeated(f, n):
+    """From https://stackoverflow.com/questions/22921626/how-to-compose-to-functions-several-times"""
+    def repeat(arg):
+        return functools.reduce(lambda r, g: g(r), [f] * n, arg)
+    return repeat
 
 
 def identity(matrix):
@@ -48,12 +69,12 @@ def rotate_90(matrix):
 
 def rotate_180(matrix):
     """(np.array -> np.array)"""
-    return compose2(rotate_90, rotate_90)(matrix)
+    return repeated(rotate_90, 2)(matrix)
 
 
 def rotate_270(matrix):
     """(np.array -> np.array)"""
-    return compose2(rotate_90, rotate_180)(matrix)
+    return repeated(rotate_90, 3)(matrix)
 
 
 def flip_horizontal(matrix):
@@ -70,29 +91,12 @@ def flip_vertical(matrix):
     return matrix @ flip_matrix
 
 
-def in_out(ins_in_out: list, flips: list, rotations: list, matrix):
-    """(list, list, list, np.array -> np.array)"""
-    for flip, rotation in itertools.product(flips, rotations):
-        new_matrix = compose2(flip, rotation)(matrix)
-        for inpt, output in ins_in_out:
-            if np.array_equal(new_matrix, inpt):
-                return output
-
-
-def block_matrix(matrix, height: int, width: int, i: int, j: int):
-    """(np.array, int, int, int, int -> np.array)"""
-    new_matrix = np.zeros((height, width))
-    for x, y in itertools.product(range(height), range(width)):
-        new_matrix[x, y] = matrix[x + i * height, y + j * width]
-    return new_matrix
-
-
 def slicing(matrix, width: int, height: int) -> list:
     """(np.array, integer, integer -> list)"""
     dim_x, dim_y = matrix.shape
     nr_slices_x: int = dim_x // height
     nr_slices_y: int = dim_y // width
-    return [[block_matrix(matrix, height, width, i, j) for j in range(nr_slices_y)] for i in range(nr_slices_x)]
+    return ((matrix[i*height:(i+1)*height, j*width:(j+1)*width] for j in range(nr_slices_y)) for i in range(nr_slices_x))
 
 
 def gluing_horizontal2(matrix1, matrix2):
@@ -102,20 +106,16 @@ def gluing_horizontal2(matrix1, matrix2):
     dim_y: int = dim_y_1 + dim_y_2
     if dim_x_1 == dim_x_2:
         dim_x: int = dim_x_1
-        new_matrix = np.zeros((dim_x, dim_y), dtype=np.dtype(np.int16))
-        for x, y in itertools.product(range(dim_x), range(dim_y)):
-            if y < dim_y_1:
-                new_matrix[x, y] = matrix1[x, y]
-            else:
-                new_matrix[x, y] = matrix2[x, y-dim_y_1]
-        return new_matrix
+        return np.array([[matrix1[x, y] if y < dim_y_1 else matrix2[x, y-dim_y_1] for y in range(dim_y)] for x in range(dim_x)])
     else:
         return "Different heights!"
 
 
-def gluing_horizontal(*matrices):
+@timer
+def gluing_horizontal(matrices):
     """(list -> np.array)"""
     return functools.reduce(gluing_horizontal2, matrices)
+
 
 
 def gluing_vertical2(matrix1, matrix2):
@@ -125,28 +125,22 @@ def gluing_vertical2(matrix1, matrix2):
     dim_x: int = dim_x_1 + dim_x_2
     if dim_y_1 == dim_y_2:
         dim_y: int = dim_y_1
-        new_matrix = np.zeros((dim_x, dim_y), dtype=np.dtype(np.int16))
-        for x, y in itertools.product(range(dim_x), range(dim_y)):
-            new_matrix[x, y] = matrix1[x,
-                                       y] if x < dim_x_1 else matrix2[x-dim_x_1, y]
-        return new_matrix
+        return np.array([[matrix1[x, y] if x < dim_x_1 else matrix2[x-dim_x_1, y] for y in range(dim_y)] for x in range(dim_x)])
     else:
         return "Different widths!"
 
 
-def gluing_vertical(*matrices):
+def gluing_vertical(matrices):
     """(list -> np.array)"""
     return functools.reduce(gluing_vertical2, matrices)
 
 
 def gluing(list_list_matrices: list):
     """(list -> np.array)"""
-    new_list_matrices = [gluing_horizontal(
-        *list_matrices) for list_matrices in list_list_matrices]
-    return gluing_vertical(*new_list_matrices)
+    return gluing_vertical(gluing_horizontal(list_matrices) for list_matrices in list_list_matrices)
 
-
-def iteration(ins_in_out: list, flips: list, rotations: list, matrix):
+@timer
+def iteration(ins_in_out: list, matrix):
     """(list, list, list, np.array -> np.array)"""
     size, _ = matrix.shape
     slices: list
@@ -154,31 +148,21 @@ def iteration(ins_in_out: list, flips: list, rotations: list, matrix):
         slices = slicing(matrix, 2, 2)
     elif not size % 3:
         slices = slicing(matrix, 3, 3)
-    trans_slices: list = [[in_out(ins_in_out, flips, rotations, mat)
-                           for mat in mat_slice] for mat_slice in slices]
+    trans_slices = ((ins_in_out[tuple(map(tuple, mat))]
+                     for mat in mat_slice) for mat_slice in slices)
     return gluing(trans_slices)
-
-
-def data_input(filename: str) -> list:
-    """"""
-    with open(filename) as f:
-        file = f.read()
-        file = file.replace("#", "1")
-        file = file.replace(".", "0")
-        file: List[str] = file.split("\n")
-        input_reg_ex: str = r"(\d+)\/(\d+)\/?(\d*)? "
-        output_reg_ex: str = r" (\d+)\/(\d+)\/(\d+)\/?(\d*)?"
-        return [(create_matrix(dataline, input_reg_ex), create_matrix(dataline, output_reg_ex)) for dataline in file]
 
 
 def constructor(instructions_in_out: list, rounds: int) -> int:
     """"""
-    flips = [identity, flip_horizontal, flip_vertical]
-    rotations = [identity, rotate_90, rotate_180, rotate_270]
     matrix = np.array([[0, 1, 0], [0, 0, 1], [1, 1, 1]],
                       dtype=np.dtype(np.int16))
+    # counter = 0
+    # print(counter)
     for _ in range(rounds):
-        matrix = iteration(instructions_in_out, flips, rotations, matrix)
+        matrix = iteration(instructions_in_out, matrix)
+        # counter += 1
+        # print(counter)
 
     return np.count_nonzero(matrix == 1)
 
@@ -186,11 +170,11 @@ def constructor(instructions_in_out: list, rounds: int) -> int:
 @timer
 def part_1(instructions_in_out: list, rounds: int = 5) -> int:
     """"""
-    return constructor(instructions_in_out, rounds)
+    return constructor(complete_instruction_dict(instructions_in_out), rounds)
 
 
 @timer
 def part_2(instructions_in_out: list) -> int:
     """"""
     rounds: int = 18
-    return constructor(instructions_in_out, rounds)
+    return constructor(complete_instruction_dict(instructions_in_out), rounds)
--- libs/timer.py
+++ libs/timer.py
@@ -2,15 +2,17 @@
 
 import functools
 import time
+from typing import Callable
 
 
-def timer(func):
+def timer(func: Callable) -> Callable:
+    """https://realpython.com/python-timer/#creating-a-python-timer-decorator"""
     @functools.wraps(func)
     def wrapper_timer(*args, **kwargs):
         tic = time.perf_counter()
         value = func(*args, **kwargs)
         toc = time.perf_counter()
         elapsed_time = toc - tic
-        print(f"Elapsed time: {elapsed_time:0.4f} seconds")
+        print(f"Elapsed time ({func.__name__}): {elapsed_time:0.4f} seconds")
         return value
     return wrapper_timer
